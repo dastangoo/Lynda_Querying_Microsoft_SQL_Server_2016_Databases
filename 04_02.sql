7567b09f8ab36d2a979c30882583878df080d862
Hamed Dastangoo <hamed.dastangoo@gmail.com>
2017-07-17 21:09:32 +0430

    Understanding table relationship

---
+++ 04_02.sql
@@ -0,0 +1,8 @@
+USE AdventureWorks2014;
+GO
+
+SELECT per.FirstName, per.LastName, hre.LoginID
+FROM Person.Person AS per
+JOIN HumanResources.Employee AS hre ON hre.BusinessEntityID = per.BusinessEntityID;
+GO
+
