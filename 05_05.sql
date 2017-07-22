81c7fa7eaebbd8ca83deff4fa46e5c74410b582d
Hamed Dastangoo <hamed.dastangoo@gmail.com>
2017-07-22 15:26:08 +0430

    Just some tweaks

--- 05_05.sql
+++ 05_05.sql
@@ -8,3 +8,41 @@ GO
 SELECT *
 FROM HumanResources.Employee;
 GO
+
+SELECT COUNT(DISTINCT JobTitle) AS [Number of Titles]
+FROM HumanResources.Employee;
+GO
+
+SELECT COUNT( JobTitle) AS [Number of Titles]
+FROM HumanResources.Employee;
+GO
+
+SELECT AVG(VacationHours) AS [Avg Vacation Hours] 
+FROM HumanResources.Employee;
+GO
+
+SELECT MAX(VacationHours) AS [Max Vacation]
+FROM HumanResources.Employee;
+GO
+
+SELECT MIN(VacationHours) AS [Min Vacation]
+FROM HumanResources.Employee;
+GO
+
+SELECT SUM(VacationHours) AS [Total Vacation]
+FROM HumanResources.Employee;
+GO
+
+SELECT VAR(VacationHours) AS [Variance]
+FROM HumanResources.Employee;
+GO
+
+SELECT MIN(VacationHours) AS [MIN Vacation],
+	MAX(VacationHours) AS [MAX Vacation],
+	SUM(VacationHours) AS [SUM Vacation],
+	SUM(SickLeaveHours) AS [SUM SickLeave],
+	MAX(HireDate) AS [Last Hire Date]
+FROM HumanResources.Employee;
+GO
+
+
